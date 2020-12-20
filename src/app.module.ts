5ea5784d559083c93bc2243cd7ad62b4b06a4a28
seokh1213 <seokh1213@gmail.com>
2020-12-20 13:48:25 +0900

    connect postgresql by typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { RestaurantsModule } from './restaurants/restaurants.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'seokhyun',
+      password: '12345',
+      database: 'nuber-eats',
+      synchronize: true,
+      logging: true,
+    }),
     GraphQLModule.forRoot({
       //autoSchemaFile: join(process.cwd(), 'src/schema.gql') // file,
       autoSchemaFile: true, // memory
