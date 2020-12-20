010698b7dddc1b8bb7d4dfa6ed7b4ca781ccc423
seokh1213 <seokh1213@gmail.com>
2020-12-21 08:23:34 +0900

    create entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,3 +1,4 @@
+import { Restaurant } from './restaurants/entities/restaurant.entity';
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { RestaurantsModule } from './restaurants/restaurants.module';
@@ -27,8 +28,9 @@ import * as Joi from 'joi';
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
-      synchronize: true,
+      synchronize: process.env.NODE_ENV !== 'prod',
       logging: true,
+      entities: [Restaurant],
     }),
     GraphQLModule.forRoot({
       //autoSchemaFile: join(process.cwd(), 'src/schema.gql') // file,
