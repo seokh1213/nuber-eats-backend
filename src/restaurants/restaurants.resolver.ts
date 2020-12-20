c5cbff3efce29abddfd9ae51c5bcc5ba54dd8f61
seokh1213 <seokh1213@gmail.com>
2020-12-20 11:53:30 +0900

    init and graphql test

--- src/restaurants/restaurants.resolver.ts
+++ src/restaurants/restaurants.resolver.ts
@@ -1,9 +1,17 @@
-import { Resolver, Query } from '@nestjs/graphql';
+import { createRestaurantDto } from './dtos/create-restaurant.dto';
+import { Resolver, Query, Args, Mutation } from '@nestjs/graphql';
+import { Restaurant } from './entities/restaurant.entity';
 
 @Resolver()
 export class RestaurantResolver {
-  @Query(() => Boolean)
-  isPizzaGood() {
+  @Query((returns) => [Restaurant])
+  restaurants(@Args('veganOnly') veganOnly: boolean): Restaurant[] {
+    return [];
+  }
+
+  @Mutation((returns) => Boolean)
+  createRestaurant(@Args() createRestaurantDto: createRestaurantDto): boolean {
+    console.log(createRestaurantDto);
     return true;
   }
 }
